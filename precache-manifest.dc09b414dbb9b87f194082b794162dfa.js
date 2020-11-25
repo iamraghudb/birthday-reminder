3f39299165ab318c7ad3405d309fc34f5e9d37cb
iamraghudb <54179651+iamraghudb@users.noreply.github.com>
2020-11-25 13:23:26 +0530

    Updates

---
+++ precache-manifest.dc09b414dbb9b87f194082b794162dfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453431c251bf72f384bb7ab86dbeea0d",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "4170e19c2f13637efd0b",
+    "url": "/birthday-reminder/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4170e19c2f13637efd0b",
+    "url": "/birthday-reminder/static/js/main.63d534ff.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
